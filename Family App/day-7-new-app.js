95df901a74995e5c7ea880568b0d05701f85becf
Bsadashiv123 <bsadashiv123@gmail.com>
2015-03-23 22:42:29 +0530

    deleted functionality

--- Family App/day-7-new-app.js
+++ Family App/day-7-new-app.js
@@ -9,27 +9,30 @@ app.controller('familyController', function ($scope) {
 		  
 		  
 		  
-		  //members static list
+		  ////////members static list
+		  
 		  $scope.members = [{id:0, firstname:'Prakash', lastname:'Bachuwar', age:65, gender:'Male'},
 		                  {id:1, firstname:'Chandrakala', lastname:'Bachuwar', age:55, gender:'Female'} ]
 						 
-		  //members new list
+		  
+		  ////////members new list
+		  
 	      $scope.savecreate = function(gmodel){ //added gmodel for getting form ng-models to controller
 	      $scope.viewPage = true;
 		  $scope.members.push({firstname: gmodel.firstname, lastname:gmodel.lastname, age:gmodel.age, gender:gmodel.gender})	
-		  
-		  $scope.familyLength = function ($scope) {return $scope.members.length;}
 		  }
 		  
 		  
-		  //delete member 
-		  $scope.selectMember = false;
+		  ////////deleting member 
 		  $scope.deleteMember = function (id) {
-	                //alert($scope.members[id].firstname)	
-					
-					if($scope.selectMember === true) {
-						  alert("test")
-						}	  
+	               $scope.members.splice([id], 1)	
+		  }
+		  
+		  
+		   ////////editing member 
+		   
+		  $scope.editMember = function (id) {
+	               $scope.editnames = true;	
 		  }
 		  
 		  
